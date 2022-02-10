29061d5426a5798d0a3bfaecf9f1881a9a5641b5
Simon Gaudek <simon.gaudek@team-con.de>
2022-02-10 13:14:22 +0100

    fix(app): Fix home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
+  <div style="text-align: center">
     <h1>T.Run 2022</h1>
-    <p>Der T.Run startet wieder im März 2022.</p>
+    <p>Der T.Run startet wieder im April 2022.</p>
+    <iframe
+      src="https://giphy.com/embed/2bUpP71bbVnZ3x7lgQ"
+      width="580"
+      height="501"
+      frameBorder="0"
+      style="margin: 100px"
+      allowFullScreen
+    ></iframe>
   </div>
 </template>
 
