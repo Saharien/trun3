30ecab69d3dc537fea2fff324197bd859b287c3d
Simon Gaudek <simon.gaudek@team-con.de>
2022-03-22 15:46:49 +0100

    feat(api): Use basic auth for trigger functions

--- api/createDummyActivityManualTrigger/index.ts
+++ api/createDummyActivityManualTrigger/index.ts
@@ -1,14 +1,23 @@
 import { Context, HttpRequest } from "@azure/functions";
 import createDummyActivity from "../createDummyActivity";
 import { buildResponseContext } from "../lib/context";
-import { verifyToken } from "../lib/jwt";
 
 export default async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
   try {
-    await verifyToken(req);
+    const auth = req.headers["authorization"];
+    if (!auth) throw new Error();
+
+    const creds = Buffer.from(auth.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    const username = creds[0];
+    const password = creds[1];
+
+    if (username !== process.env.cronUser && password !== process.env.cronPass)
+      throw new Error();
   } catch (error) {
     context.res = buildResponseContext({ status: 401, message: error.message });
     return;
