eaf1b30a6afb2f2144e3b2aecf151285f706a71c
Simon Gaudek <simon.gaudek@team-con.de>
2022-03-31 11:30:25 +0200

    fix(api): Add filter to longest bikings and runs

--- api/runLongest/index.ts
+++ api/runLongest/index.ts
@@ -4,6 +4,9 @@ import { Model as Run } from "../lib/run.model";
 import { verifyToken } from "../lib/jwt";
 import { buildResponseContext } from "../lib/context";
 
+const tRunYear: number =
+  parseInt(process.env.TRunYear) || new Date().getFullYear();
+
 export default async function (
   context: Context,
   req: HttpRequest
@@ -21,6 +24,13 @@ export default async function (
     return;
   }
 
-  const longestRides = await Run.find().sort("-distance").limit(10);
-  context.res = buildResponseContext({ data: longestRides });
+  const longestRuns = await Run.find({
+    date: {
+      $gte: new Date(`${tRunYear}-04-01`),
+      $lt: new Date(`${tRunYear}-07-01`),
+    },
+  })
+    .sort("-distance")
+    .limit(10);
+  context.res = buildResponseContext({ data: longestRuns });
 }
